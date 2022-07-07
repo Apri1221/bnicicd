34c25fbc7aabd8dd858edc23c533705803fa87c9
unknown <apriyantotobing8@gmail.com>
2022-07-07 14:46:34 +0700

    adding directory

--- bnicicd.py
+++ bnicicd.py
@@ -1,8 +1,13 @@
 import requests
+from datetime import datetime
 
 print("hello world")
 print("hello Pria")
 
 response = requests.get("https://www.google.com")
 
-print(response.text)
+# print(response.text)
+
+waktu = datetime.now()
+with open("tempResponse/" + str(waktu) + ".txt" , "w") as f:
+    f.write(response.txt)
